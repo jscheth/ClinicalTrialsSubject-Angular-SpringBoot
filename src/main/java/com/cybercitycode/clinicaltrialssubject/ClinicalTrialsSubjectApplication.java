92acbeee462e321bdbdac4edea4c17f347350201
jscheth <sovichea.chet@gmail.com>
2025-03-22 17:55:58 -0700

    Init Commit

---
+++ src/main/java/com/cybercitycode/clinicaltrialssubject/ClinicalTrialsSubjectApplication.java
@@ -0,0 +1,13 @@
+package com.cybercitycode.clinicaltrialssubject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicalTrialsSubjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicalTrialsSubjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cybercitycode/clinicaltrialssubject/ClinicalTrialsSubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybercitycode.clinicaltrialssubject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalTrialsSubjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
